4c501bd438058e5cb8764c7c59f934b1f749c709
sharontay <sharontaymarin@gmail.com>
2025-06-11 19:57:45 +0200

    Create Author entity

---
+++ LibraryAPI/Entities/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.Entities
+{
+    public class Author
+    {
+        public string AuthorId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
