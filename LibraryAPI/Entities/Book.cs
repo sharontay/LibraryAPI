6b623d62d8da212f140a09395aa054f54a409d5c
sharontay <sharontaymarin@gmail.com>
2025-06-11 19:36:09 +0200

    Create Book Entity

---
+++ LibraryAPI/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.Entities
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int PageCount { get; set; }
+    }
+}
