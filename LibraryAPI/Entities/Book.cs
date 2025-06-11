571b7b1aff6a471b408d80ffb52f4a5382e50e0f
sharontay <sharontaymarin@gmail.com>
2025-06-11 19:46:09 +0200

    Create Publisher entity

--- LibraryAPI/Entities/Book.cs
+++ LibraryAPI/Entities/Book.cs
@@ -2,9 +2,10 @@
 {
     public class Book
     {
-        public int BookId { get; set; }
+        public string BookId { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int PageCount { get; set; }
+        public int YearPublished { get; set; }
     }
 }
